ffdec335f21d5b512d341037048701a8fde7c7a3
samirraihan <samirraihan93@gmail.com>
2026-02-16 17:02:22 +0600

    Add createUser method to AuthRepository for user creation functionality

--- app/Modules/Auth/Repositories/AuthRepository.php
+++ app/Modules/Auth/Repositories/AuthRepository.php
@@ -6,6 +6,11 @@ use App\Models\User;
 
 class AuthRepository
 {
+    public function createUser(array $data)
+    {
+        return User::create($data);
+    }
+    
     public function findByEmail($email)
     {
         return User::where('email', $email)->first();
