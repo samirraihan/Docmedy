f21eb0d56368312d7b78a12841e17364e69a9ff1
samirraihan <samirraihan93@gmail.com>
2026-02-20 17:44:57 +0600

    Refactor ModuleRouteServiceProvider to streamline register and boot methods

--- app/Providers/ModuleRouteServiceProvider.php
+++ app/Providers/ModuleRouteServiceProvider.php
@@ -8,30 +8,38 @@ use Illuminate\Support\Facades\File;
 
 class ModuleRouteServiceProvider extends ServiceProvider
 {
+    public function register(): void
+    {
+        $modules = File::directories(app_path('Modules'));
+
+        foreach ($modules as $module) {
+
+            $moduleName = basename($module);
+
+            $provider =
+                "App\\Modules\\{$moduleName}\\Providers\\{$moduleName}ServiceProvider";
+
+            if (class_exists($provider)) {
+                $this->app->register($provider);
+            }
+        }
+    }
+
     public function boot(): void
     {
-        $modulesPath = app_path('Modules');
-
-        if (!File::exists($modulesPath)) {
-            return;
-        }
-
-        $modules = File::directories($modulesPath);
+        $modules = File::directories(app_path('Modules'));
 
         foreach ($modules as $module) {
 
             $routesPath = $module . '/Routes';
 
-            if (!File::exists($routesPath)) {
-                continue;
-            }
+            if (!File::exists($routesPath)) continue;
 
-            // Detect versions automatically (V1, V2, V3...)
             $versions = File::directories($routesPath);
 
-            foreach ($versions as $versionPath) {
+            foreach ($versions as $version) {
 
-                $apiFile = $versionPath . '/api.php';
+                $apiFile = $version . '/api.php';
 
                 if (File::exists($apiFile)) {
                     Route::middleware('api')
