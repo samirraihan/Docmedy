5f257c2163891a099725e6851679c6af8c71ec98
samirraihan <samirraihan93@gmail.com>
2026-02-21 13:01:37 +0600

    Implement AuthRepositoryInterface in AuthRepository for better adherence to contract

--- app/Modules/Auth/Repositories/AuthRepository.php
+++ app/Modules/Auth/Repositories/AuthRepository.php
@@ -4,8 +4,9 @@ namespace App\Modules\Auth\Repositories;
 
 use App\Core\Base\BaseRepository;
 use App\Models\User;
+use App\Modules\Auth\Interfaces\AuthRepositoryInterface;
 
-class AuthRepository extends BaseRepository
+class AuthRepository extends BaseRepository implements AuthRepositoryInterface
 {
     public function __construct(User $model)
     {
