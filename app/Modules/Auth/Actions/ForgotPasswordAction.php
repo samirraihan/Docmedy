a0298ec7c70e8b39edeb5f10ef2712d542a0fe86
samirraihan <samirraihan93@gmail.com>
2026-02-21 13:29:32 +0600

    Add ForgotPasswordAction class to handle password reset functionality

---
+++ app/Modules/Auth/Actions/ForgotPasswordAction.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Modules\Auth\Actions;
+
+class ForgotPasswordAction
+{
+    public function execute($email)
+    {
+        // later OTP / mail integration
+        return true;
+    }
+}
